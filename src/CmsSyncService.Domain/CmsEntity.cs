d9b3f9ef010dc489d80359ca33bd6ecf35e142c7
UncleZeic <41185719+UncleZeic@users.noreply.github.com>
2026-03-29 19:27:31 +0300

    Add CmsEntity domain class and update project references for domain layer

---
+++ src/CmsSyncService.Domain/CmsEntity.cs
@@ -0,0 +1,21 @@
+namespace CmsSyncService.Domain
+{
+    public class CmsEntity
+    {
+        public Guid Id { get; set; }
+        public uint Version { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool Published { get; set; }
+
+        public bool AdminDisabled { get; set; }
+
+        protected CmsEntity()
+        {
+            Id = Guid.NewGuid();
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
+
